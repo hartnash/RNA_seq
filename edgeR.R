ef4196f32c61fb361b9dcecb17dd0df45cc5dfcf
mohitmahey <62173533+mohitmahey@users.noreply.github.com>
2023-08-14 21:52:34 -0400

    Update edgeR.R

--- edgeR.R
+++ edgeR.R
@@ -1,15 +1,22 @@
 # code written by Sarah E. Holmes and modified by Mohit Mahey for DGE analysis
 
-
+# Clearing the environment 
 options(error = recover)
 rm(list=ls())
+
+# Setting the working directory
 setwd("~/MSU REU/poa counts")
 
+# loading required libraries
 library(edgeR)
 
+# reading in the raw counts table
 x <- read.table("combines_counts.txt", header = TRUE, row.names = "GeneID", sep = '\t')
+
+# checking the counts file and its layout
 View(x)
 
+# legend for samples and their respective numbers
 #1 = A
 #2 = B
 #3 = FL
@@ -53,6 +60,8 @@ plotQLDisp(fit)
 
 plotMDS(y)
 
+# comparison of the counts and writing results to .txt files
+
 qlf.AllSvA <- glmQLFTest(fit, contrast = c(-1,0,0,1))
 write.table(qlf.AllSvA$table, "AllSvA.txt")
 AllSvA <- read.table("AllSvA.txt")
@@ -69,63 +78,59 @@ AllSvFL <- read.table("AllSvFL.txt")
 View(AllSvFL)
 
 
-
 qlf.SvR <- glmQLFTest(fit, contrast = c(-1,1))
 write.table(qlf.SvR$table, "SvR.txt")
 SvR <- read.table("SvR.txt")
 View(SvR)
 
 
-#qlf.S1vA <- glmQLFTest(fit, contrast = c(-1,0,0,1,0,0))
-#write.table(qlf.S1vA$table, "S1vA.txt")
-#S1vA <- read.table("S1vA.txt")
-#View(S1vA)
+qlf.S1vA <- glmQLFTest(fit, contrast = c(-1,0,0,1,0,0))
+write.table(qlf.S1vA$table, "S1vA.txt")
+S1vA <- read.table("S1vA.txt")
+View(S1vA)
 
-#qlf.S1vB <- glmQLFTest(fit, contrast = c(0,-1,0,1,0,0))
-#write.table(qlf.S1vB$table, "S1vB.txt")
-#S1vB <- read.table("S1vB.txt")
-#View(S1vB)
+qlf.S1vB <- glmQLFTest(fit, contrast = c(0,-1,0,1,0,0))
+write.table(qlf.S1vB$table, "S1vB.txt")
+S1vB <- read.table("S1vB.txt")
+View(S1vB)
 
-#qlf.S1vFL <- glmQLFTest(fit, contrast = c(0,0,-1,1,0,0))
-#write.table(qlf.S1vFL$table, "S1vFL.txt")
-#S1vFL <- read.table("S1vFL.txt")
-#View(S1vFL)
-
-
-#qlf.S3vA <- glmQLFTest(fit, contrast = c(-1,0,0,0,1,0))
-#write.table(qlf.S3vA$table, "S3vA.txt")
-#S3vA <- read.table("S3vA.txt")
-#View(S3vA)
-
-#qlf.S3vB <- glmQLFTest(fit, contrast = c(0,-1,0,0,1,0))
-#write.table(qlf.S3vB$table, "S3vB.txt")
-#S3vB <- read.table("S3vB.txt")
-#View(S3vB)
-
-#qlf.S3vFL <- glmQLFTest(fit, contrast = c(0,0,-1,0,1,0))
-#write.table(qlf.S3vFL$table, "S3vFL.txt")
-#S3vFL <- read.table("S3vFL.txt")
-#View(S3vFL)
+qlf.S1vFL <- glmQLFTest(fit, contrast = c(0,0,-1,1,0,0))
+write.table(qlf.S1vFL$table, "S1vFL.txt")
+S1vFL <- read.table("S1vFL.txt")
+View(S1vFL)
 
 
-#qlf.S5vA <- glmQLFTest(fit, contrast = c(-1,0,0,0,0,1))
-#write.table(qlf.S5vA$table, "S5vA.txt")
-#S5vA <- read.table("S5vA.txt")
-#View(S5vA)
+qlf.S3vA <- glmQLFTest(fit, contrast = c(-1,0,0,0,1,0))
+write.table(qlf.S3vA$table, "S3vA.txt")
+S3vA <- read.table("S3vA.txt")
+View(S3vA)
 
-#qlf.S5vB <- glmQLFTest(fit, contrast = c(0,-1,0,0,0,1))
-#write.table(qlf.S5vB$table, "S5vB.txt")
-#S5vB <- read.table("S5vB.txt")
-#View(S5vB)
+qlf.S3vB <- glmQLFTest(fit, contrast = c(0,-1,0,0,1,0))
+write.table(qlf.S3vB$table, "S3vB.txt")
+S3vB <- read.table("S3vB.txt")
+View(S3vB)
 
-#qlf.S5vFL <- glmQLFTest(fit, contrast = c(0,0,-1,0,0,1))
-#write.table(qlf.S5vFL$table, "S5vFL.txt")
-#S5vFL <- read.table("S5vFL.txt")
-#View(S5vFL)
+qlf.S3vFL <- glmQLFTest(fit, contrast = c(0,0,-1,0,1,0))
+write.table(qlf.S3vFL$table, "S3vFL.txt")
+S3vFL <- read.table("S3vFL.txt")
+View(S3vFL)
 
+qlf.S5vA <- glmQLFTest(fit, contrast = c(-1,0,0,0,0,1))
+write.table(qlf.S5vA$table, "S5vA.txt")
+S5vA <- read.table("S5vA.txt")
+View(S5vA)
 
+qlf.S5vB <- glmQLFTest(fit, contrast = c(0,-1,0,0,0,1))
+write.table(qlf.S5vB$table, "S5vB.txt")
+S5vB <- read.table("S5vB.txt")
+View(S5vB)
 
-#qlf.AvB <- glmQLFTest(fit, contrast = c(1,-1,0,0,0,0))
-#write.table(qlf.AvB$table, "AvB.txt")
-#AvB <- read.table("AvB.txt")
-#View(AvB)
+qlf.S5vFL <- glmQLFTest(fit, contrast = c(0,0,-1,0,0,1))
+write.table(qlf.S5vFL$table, "S5vFL.txt")
+S5vFL <- read.table("S5vFL.txt")
+View(S5vFL)
+
+qlf.AvB <- glmQLFTest(fit, contrast = c(1,-1,0,0,0,0))
+write.table(qlf.AvB$table, "AvB.txt")
+AvB <- read.table("AvB.txt")
+View(AvB)
